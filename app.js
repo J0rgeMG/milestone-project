e1cab6fee41b242f5a6746241b25a9d7652dd4f0
Jorge <J0rgeMG@live.com>
2025-03-20 15:56:06 -0500

    initialized express

---
+++ app.js
@@ -0,0 +1,9 @@
+// Third party packages
+const express = require('express');
+
+// Loading app
+const app = express();
+
+// Starting the app, 3000 as default port
+// <domain>:3000
+app.listen(3000);
