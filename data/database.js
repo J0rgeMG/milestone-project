0ed2b8868b6372b21098ddc9970d5c99046f89bd
Jorge <J0rgeMG@live.com>
2025-03-24 17:59:14 -0500

    created a conexion to mongodb

---
+++ data/database.js
@@ -0,0 +1,31 @@
+// Third packages
+const mongodb = require('mongodb');
+
+// We configure the MongoClient that will connect to our mongo databse
+const MongoClient = mongodb.MongoClient;
+
+// We connect with the database
+let database;
+
+async function connectToDatabase() {
+    const databaseUrl = 'localhost:27017';
+    const databaseName = 'online-shop';
+
+    const client = await MongoClient.connect(databaseUrl);
+    database = client.db(databaseName);
+}
+
+// funtion that exposes to the other files 
+function getDb() {
+    if (!database) {
+        throw new Error('You must connect to the database first!');
+    }
+
+    return database;
+}
+
+// Exports
+module.exports = {
+    connectToDatabase: connectToDatabase,
+    getDb: getDb
+};
