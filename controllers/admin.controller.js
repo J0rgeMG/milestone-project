3dd1ff112f67682c5dd5b4c769e5270af59a755d
Jorge <J0rgeMG@live.com>
2025-04-17 22:19:07 -0500

    finished the deleting products feature

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -63,13 +63,13 @@ async function updateProduct(req, res, next) {
 async function deleteProduct(req, res, next){
     let prodcut;
     try {
-        const product = await Product.findById(req.params.id);
+        product = await Product.findById(req.params.id);
         await prodcut.remove();
     } catch (error) {
         return next(error);
     }
 
-    res.redirect('/admin/products');
+    res.json({message: 'Deleted product!'});
 }
 
 module.exports = {
