f39f2737ca01f23f664bf6af786e9239fdb0f3ec
Jorge <J0rgeMG@live.com>
2025-04-22 14:31:19 -0500

    added various security related fixes

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -12,5 +12,13 @@ router.get('/',function(req, res) {
     res.redirect('/products');
 });
 
+// Error handling routes
+router.get('401', function(req, res){
+    res.status(401).render('shared/401');
+});
+router.get('403', function(req, res){
+    res.status(403).render('shared/403');
+});
+
 // Exports
 module.exports = router;
